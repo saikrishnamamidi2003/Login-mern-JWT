c6e4292fccbf82f7cecb247dfcbcd3f7e381c6a4
sai krishna <saikrishnamamidi2003@gmail.com>
2025-09-27 11:28:25 +0530

    fixed all the errors and all the routes are working fine and the token is also generated by jwt

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const RegisterUser = require('./model');
-const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken');
 mongoose.connect("mongodb+srv://saikrishnamamidi2003_db_user:Saikrishna%40123@cluster0.j90sct6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
 }).then(
     ()=>{
 
         console.log("db connected")
     }
-)
+).catch(err=> console.error("❌ MongoDB Error:", err));
 
 app.use(express.json());
 
 
 app.post('/register', async(req, res)=>{
+    console.log("Request Body:", req.body);  
     try{
         const {userName, email, password, confirmPassword} = req.body;
         let exist = await RegisterUser.findOne({email : email})
@@ -38,8 +39,52 @@ app.post('/register', async(req, res)=>{
         console.log(err);
         return res.status(500).send('Internal server Error')
     }
-})
+});
+
+// ...existing code...
+
+app.get('/data', (req, res) => {
+    res.send('Data route is working!');
+});
+
+// ...existing code...
+
+app.post('/login', async(req, res) => {
+    //console.log("Request Body:", req.body);  
+    try{
+        const {email, password} = req.body;
+        let exist = await RegisterUser.findOne({email});
+        if(!exist){
+            return res.status(400).send('User Not Found');
+        }
+        if(exist.password != password){
+            return res.status(400).send('Invalid');
+        }
+        let payload = {
+            user : {
+                id : exist.id
+            }
+        }
+        jwt.sign(payload, 'jwtSecret', {expiresIn:3600000}, 
+            (err, token) =>{
+                if(err) throw err;
+                return res.json({token})
+            }
+        )
+        
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).send('server Error');
+    }
+});
+
+
 
 app.listen(5000, ()=> {
     console.log('server running...')
 })
+
+
+
+
